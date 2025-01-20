933ae24c201bf66a1b686c00a9f5a0fd327351b8
Adam Lisik <adamlisik01@gmail.com>
2025-01-20 20:14:37 +0100

    some fixes, some changes, some improvements

--- WFC/README.md
+++ WFC/README.md
@@ -6,11 +6,18 @@ Wave Function Collapse (WFC) to algorytm stosowany do generowania obrazów, map 
 - Pygame 2.6.0 lub nowszy
 
 ## Uruchomienie programu
-Program uruchamiamy poleceniem python3 main.py  [DIM] [TILESET], gdzie DIM i TILESET to argumenty opcjonalne.
-- DIM oznacza wymiar siatki (będzie ona wymiaru DIMxDIM)
-- TILESET to nazwa zestawu płytek które będą wyświetlane.
+Program uruchamiamy poleceniem:
+main.py [-h] [--dim DIM] [--set SET] [--fps FPS]
 
-Domyślnie DIM=20, a TILESET='basic_set'.
+```
+Wave Function Collapse
+
+options:
+  -h, --help  show this help message and exit
+  --dim DIM   Dimention of the grid (default 20)
+  --set SET   Tileset (default 'extended_basic_set')
+  --fps FPS   FPS (default 240)
+```
 
 ## Działanie programu
 Pseudokod wygląda następująco:
@@ -21,18 +28,38 @@ Pseudokod wygląda następująco:
 5. Jeśli nie wszystkie elementy siatki uległy kolapsowi, idź do punktu 3.
 
 ### Płytki:
-Są one zdefiniowane w plikach .json w katalogu tile_sets/\<wybrany zestaw\>/tiles.json. Przykładowa definicja:
+Są one zdefiniowane w plikach .json w katalogu tile_sets/\<wybrany zestaw\>/tiles.json. Przykład: 
+
+<br>
+
+<img src="tile_sets/basic_set/images/tile-0.png" alt="Płytka" width="200" height="200">
+<br> <br>
+Powyższa płytka w pliku .json będzie opisana następująco:
+
+```
 {
-"id":  0,
+"id":  0, 
 "path":  "basic_set/images/tile-0.png",
 "north":  [0,  2],
 "east":  [0,  1],
 "south":  [0,  2],
 "west":  [0,  1]
 }
-Klucze north, east, south oraz west określają id płytek które moga sąsiadować z nasza płytką od danej strony.
+```
 
+Klucze north, east, south oraz west określają id płytek które moga sąsiadować z nasza płytką od danej strony. Przykładowa płytka zatem od północy sąsiadować może z płytkami o id 0 i 2, czyli: 
 
+<img src="tile_sets/basic_set/images/tile-0.png" alt="Płytka" width="200" height="200" style="margin-right: 25px">
+<img src="tile_sets/basic_set/images/tile-2.png" alt="Płytka" width="200" height="200">
+
+, a od wschodu z płytkami o id 0 i 1:
+
+<img src="tile_sets/basic_set/images/tile-0.png" alt="Płytka" width="200" height="200" style="margin-right: 25px">
+<img src="tile_sets/basic_set/images/tile-1.png" alt="Płytka" width="200" height="200">
+
+<br> <br>
+W każdym zestawie płytek ostatnią pozycję zajmuje "płytka pusta". Jako że w mojej implementacji każdą pozycję na planszy zajmuje obiekt którego jednym z atrybutów jest zbiór możliwych płytek na dane miejsce, to takie rozwiązanie jest wygodne do reprezentowania pozycji
+o maksymalnej entropii (można to zrobić dużo lepiej i logiczniej, ale niestety fundamenty zbudowałem na takim pomyśle więc muszę się tak ratować :c).
 
 ### Entropia
 Entropią jest liczba możliwych płytek jakie 'pasują' w danym miejscu siatki.
@@ -43,7 +70,19 @@ Kolaps odbywa się poprzez znalezienie elementu w siatce o najmniejszej entropii
 
 ## Przykładowy przebieg programu
 
- 
+Poniżej przedstawiam róne etapy wykonywania programu (przebiegi są różne):
+
+<img src="md_images\Entropia0.png" alt="Płytka" width="400" height="400">
+
+Pierwsza losowo wybrana płytka zajmuje losowe miejsce na planszy, kolejno obliczana jest entropia każdego pola na podstawie długości zbiorów możliwych płytek jakie mogą się na danym miejscu znaleźć. Po tej operacji losowo wybierane jest jedno pole ze zbioru tych o najmniejszej entropii i ostatecznie dochodzi do kolapsu, a więc wybrania konkretnej płytki ze zbioru możliwych.
+
+Dobra wiadomość jest taka, że w tym programie wiemy co to znaczy i nie musimy wymyślać eksperymentów myślowych z martwymi (a może nie?) kotami aby dobrać interpretację do rzeczywistości -> https://www.youtube.com/watch?v=yB1vpsL6eXo szczerze polecam.
+
+<img src="md_images\Entropia1.png" alt="Płytka" width="400" height="400">
+
+Na obrazku powyżej przedstawiona jest obliczona entropia. Widzimy, że jest tu tylko jedna pozycja której entropia jest równa 1 - ta pozycja więc zostanie wybrana i na niej zapisana zostanie odpowiednia płytka (krzyżyk).
+
+
 
 ## Źródła i pomocne linki
 - https://github.com/mxgmn/WaveFunctionCollapse
--- WFC/main.py
+++ WFC/main.py
@@ -2,15 +2,24 @@ import pygame
 import random
 import json
 import sys
+import argparse
 
 import time
 
 # pygame initial setup:
 SCREEN_SIZE = (900, 900)
-if len(sys.argv) > 1:
-    DIM = int(sys.argv[1])
-else:
-    DIM = 20
+
+parser = argparse.ArgumentParser(description="Wave Function Collapse")
+parser.add_argument("--dim", type=int, default=20, help="Dimention of the grid (default 20)")
+parser.add_argument("--set", type=str, default="extended_basic_set", help="Tileset (default 'extended_basic_set')")
+parser.add_argument("--fps", type=int, default=240, help="FPS (default 240)")
+
+args = parser.parse_args()
+
+DIM = args.dim
+TILES = f'tile_sets/{args.set}/tiles.json'
+FPS = args.fps
+
 CELL_SIZE = (SCREEN_SIZE[0] / DIM, SCREEN_SIZE[1] / DIM)
 
 pygame.init()
@@ -18,13 +27,6 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("Wave Function Collapse")
 clock = pygame.time.Clock()
 running = True
-
-
-# WFC tiles:
-if len(sys.argv) > 2:
-    TILES = 'tile_sets/' + sys.argv[2] + '/tiles.json'
-else:
-    TILES = 'tile_sets/basic_set/tiles.json'
 
 with open(TILES, 'r') as file:
     tiles = json.load(file)
@@ -52,50 +54,97 @@ class GridCell:
 
 
     def __str__(self):
-        return f'GridCell({self.x}, {self.y}, {self.entrophy}, {self.possibilities}, {self.tile}, {self.collapsed})'
-    
+        return (
+            f"GridCell("
+            f"x={self.x}, y={self.y}, "
+            f"entrophy={self.entrophy}, "
+            f"possibilities={self.possibilities}, "
+            f"tile={self.tile}, "
+            f"collapsed={self.collapsed})"
+        )
+
     def __repr__(self):
-        return f'GridCell({self.x}, {self.y}, {self.entrophy}, {self.possibilities}, {self.tile}, {self.collapsed})'
+        return (
+            f"GridCell("
+            f"x={self.x}, y={self.y}, "
+            f"entrophy={self.entrophy}, "
+            f"possibilities={self.possibilities}, "
+            f"tile={self.tile}, "
+            f"collapsed={self.collapsed})"
+        )
 
 WTCgrid = [[GridCell(x, y)
             for y in range(DIM)] for x in range(DIM)]
 
-def draw_tile(tile, x, y):
+def draw_tile(tile: int, x: int, y: int) -> None:
     screen.blit(tiles[tile]["image"], (CELL_SIZE[0]*y, CELL_SIZE[1]*x))
     WTCgrid[x][y].collapsed = True
     WTCgrid[x][y].tile = tile
 
 
-def calculate_entrophy(cell):
+def calculate_entrophy(cell: GridCell) -> tuple:
     possibilities_set = set(range(len(tiles) - 1))
     if cell.collapsed:
         return (0, set())
     else:
         if cell.x == 0:
             if cell.y == 0:
-                possibilities_set = tiles[WTCgrid[0][1].tile]['west'] & tiles[WTCgrid[1][0].tile]['north']
+                possibilities_set = (
+                    tiles[WTCgrid[0][1].tile]['west'] &
+                    tiles[WTCgrid[1][0].tile]['north']
+                )
             elif cell.y == DIM - 1:
-                possibilities_set = tiles[WTCgrid[0][DIM - 2].tile]['east'] & tiles[WTCgrid[1][DIM - 1].tile]['north']
+                possibilities_set = (
+                    tiles[WTCgrid[0][DIM - 2].tile]['east'] &
+                    tiles[WTCgrid[1][DIM - 1].tile]['north']
+                )
             else:
-                possibilities_set = tiles[WTCgrid[0][cell.y - 1].tile]['east'] & tiles[WTCgrid[0][cell.y + 1].tile]['west'] & tiles[WTCgrid[1][cell.y].tile]['north']
+                possibilities_set = (
+                    tiles[WTCgrid[0][cell.y - 1].tile]['east'] &
+                    tiles[WTCgrid[0][cell.y + 1].tile]['west'] &
+                    tiles[WTCgrid[1][cell.y].tile]['north']
+                )
         elif cell.x == DIM - 1:
             if cell.y == 0:
-                possibilities_set = tiles[WTCgrid[DIM - 1][1].tile]['west'] & tiles[WTCgrid[DIM - 2][0].tile]['south']
+                possibilities_set = (
+                    tiles[WTCgrid[DIM - 1][1].tile]['west'] &
+                    tiles[WTCgrid[DIM - 2][0].tile]['south']
+                )
             elif cell.y == DIM - 1:
-                possibilities_set = tiles[WTCgrid[DIM - 1][DIM - 2].tile]['east'] & tiles[WTCgrid[DIM - 2][DIM - 1].tile]['south']
+                possibilities_set = (
+                    tiles[WTCgrid[DIM - 1][DIM - 2].tile]['east'] &
+                    tiles[WTCgrid[DIM - 2][DIM - 1].tile]['south']
+                )
             else:
-                possibilities_set = tiles[WTCgrid[DIM - 1][cell.y - 1].tile]['east'] & tiles[WTCgrid[DIM - 1][cell.y + 1].tile]['west'] & tiles[WTCgrid[DIM - 2][cell.y].tile]['south']
+                possibilities_set = (
+                    tiles[WTCgrid[DIM - 1][cell.y - 1].tile]['east'] &
+                    tiles[WTCgrid[DIM - 1][cell.y + 1].tile]['west'] &
+                    tiles[WTCgrid[DIM - 2][cell.y].tile]['south']
+                )
         elif cell.y == 0:
-            possibilities_set = tiles[WTCgrid[cell.x - 1][0].tile]['south'] & tiles[WTCgrid[cell.x + 1][0].tile]['north'] & tiles[WTCgrid[cell.x][1].tile]['west']
-
+            possibilities_set = (
+                tiles[WTCgrid[cell.x - 1][0].tile]['south'] &
+                tiles[WTCgrid[cell.x + 1][0].tile]['north'] &
+                tiles[WTCgrid[cell.x][1].tile]['west']
+            )
         elif cell.y == DIM - 1:
-            possibilities_set = tiles[WTCgrid[cell.x - 1][DIM - 1].tile]['south'] & tiles[WTCgrid[cell.x + 1][DIM - 1].tile]['north'] & tiles[WTCgrid[cell.x][DIM - 2].tile]['east']
+            possibilities_set = (
+                tiles[WTCgrid[cell.x - 1][DIM - 1].tile]['south'] &
+                tiles[WTCgrid[cell.x + 1][DIM - 1].tile]['north'] &
+                tiles[WTCgrid[cell.x][DIM - 2].tile]['east']
+            )
         else:
-            possibilities_set = tiles[WTCgrid[cell.x - 1][cell.y].tile]['south'] & tiles[WTCgrid[cell.x + 1][cell.y].tile]['north'] & tiles[WTCgrid[cell.x][cell.y - 1].tile]['east']  & tiles[WTCgrid[cell.x][cell.y + 1].tile]['west'] 
+            possibilities_set = (
+                tiles[WTCgrid[cell.x - 1][cell.y].tile]['south'] &
+                tiles[WTCgrid[cell.x + 1][cell.y].tile]['north'] &
+                tiles[WTCgrid[cell.x][cell.y - 1].tile]['east'] &
+                tiles[WTCgrid[cell.x][cell.y + 1].tile]['west']
+            )
 
         return (len(possibilities_set), possibilities_set)
+
     
-def pick_and_collapse():
+def pick_and_collapse() -> None:
     min = len(tiles) - 1
     smallest_ent = []
     for x in range(DIM):
@@ -114,13 +163,15 @@ def pick_and_collapse():
         return
     smallest_ent_pick = random.choice(smallest_ent)
     cell_position = smallest_ent_pick[1]
-    cell_tile = random.sample(smallest_ent_pick[0][1], 1)[0]
+    cell_tile = random.sample(list(smallest_ent_pick[0][1]), 1)[0]
     draw_tile(cell_tile, cell_position[0], cell_position[1])
 
     return smallest_ent
 
 
-draw_tile(random.choice(range(len(tiles) - 1)), 0, DIM-1)
+draw_tile(random.choice(range(len(tiles) - 1)), 
+          random.randint(0, DIM - 1), 
+          random.randint(0, DIM - 1))
 
 N = DIM * DIM
 i = 1
@@ -134,11 +185,10 @@ while running:
             if event.key == pygame.K_s:
                 time.sleep(1000000)
 
-    # screen.fill("black")
     if i < N:
         pick_and_collapse()
         i += 1
 
     pygame.display.flip()
 
-    clock.tick(240)
+    clock.tick(FPS)
